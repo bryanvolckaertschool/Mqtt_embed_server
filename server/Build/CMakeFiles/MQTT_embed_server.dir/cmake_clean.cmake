6df4122d26a32f20a0f1b87a094743fed225a1e6
Bryan Volckaert <bryan.volckaert@student.vives.be>
2021-11-09 09:30:32 +0000

    init

---
+++ server/Build/CMakeFiles/MQTT_embed_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MQTT_embed_server.dir/main.c.o"
+  "MQTT_embed_server"
+  "MQTT_embed_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MQTT_embed_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ website/Build/CMakeFiles/ENV-CGI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/usr/lib/cgi-bin/ENV-CGI"
+  "/usr/lib/cgi-bin/ENV-CGI.pdb"
+  "CMakeFiles/ENV-CGI.dir/env-cgi.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ENV-CGI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
